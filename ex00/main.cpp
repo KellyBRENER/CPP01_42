c9310cd4123637a4cfd9267085982c88619965c5
Kelly Brener-Raffalli <kbrener-@c3r1p4.42perpignan.fr>
2024-10-17 14:39:46 +0200

    ex00_done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,15 +6,16 @@
 /*   By: kbrener- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 11:07:02 by kbrener-          #+#    #+#             */
-/*   Updated: 2024/10/17 12:04:55 by kbrener-         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:38:04 by kbrener-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int	main(void) {
-	Zombie	*first_Zombie = newZombie("Foo");
-	first_Zombie->announce();
-	delete first_Zombie;
-	first_Zombie = nullptr;
+	Zombie	*allocated_Zombie = newZombie("Foo");
+	allocated_Zombie->announce();
+	randomChump("non_allocated Zombie");
+	delete allocated_Zombie;
+	allocated_Zombie = NULL;
 }
